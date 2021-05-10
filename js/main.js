5424e7b188ac751a0704729e27d752243fa332cf
arunmoorthattil <43172183+arunmoorthattil@users.noreply.github.com>
2021-05-10 16:52:02 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -228,96 +228,6 @@ function evaluateBoard (move, prevSum, color)
     return prevSum;
 }
 
-/*
- * Performs the minimax algorithm to choose the best move: https://en.wikipedia.org/wiki/Minimax (pseudocode provided)
- * Recursively explores all possible moves up to a given depth, and evaluates the game board at the leaves.
- * 
- * Basic idea: maximize the minimum value of the position resulting from the opponent's possible following moves.
- * Optimization: alpha-beta pruning: https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning (pseudocode provided)
- * 
- * Inputs:
- *  - game:                 the game object.
- *  - depth:                the depth of the recursive tree of all possible moves (i.e. height limit).
- *  - isMaximizingPlayer:   true if the current layer is maximizing, false otherwise.
- *  - sum:                  the sum (evaluation) so far at the current layer.
- *  - color:                the color of the current player.
- * 
- * Output:
- *  the best move at the root of the current subtree.
- */
-function minimax(game, depth, alpha, beta, isMaximizingPlayer, sum, color)
-{
-    positionCount++; 
-    var children = game.ugly_moves({verbose: true});
-    
-    // Sort moves randomly, so the same move isn't always picked on ties
-    children.sort(function(a, b){return 0.5 - Math.random()});
-    
-    var currMove;
-    // Maximum depth exceeded or node is a terminal node (no children)
-    if (depth === 0 || children.length === 0)
-    {
-        return [null, sum]
-    }
-
-    // Find maximum/minimum from list of 'children' (possible moves)
-    var maxValue = Number.NEGATIVE_INFINITY;
-    var minValue = Number.POSITIVE_INFINITY;
-    var bestMove;
-    for (var i = 0; i < children.length; i++)
-    {
-        currMove = children[i];
-
-        // Note: in our case, the 'children' are simply modified game states
-        var currPrettyMove = game.ugly_move(currMove);
-        var newSum = evaluateBoard(currPrettyMove, sum, color);
-        var [childBestMove, childValue] = minimax(game, depth - 1, alpha, beta, !isMaximizingPlayer, newSum, color);
-        
-        game.undo();
-    
-        if (isMaximizingPlayer)
-        {
-            if (childValue > maxValue)
-            {
-                maxValue = childValue;
-                bestMove = currPrettyMove;
-            }
-            if (childValue > alpha)
-            {
-                alpha = childValue;
-            }
-        }
-
-        else
-        {
-            if (childValue < minValue)
-            {
-                minValue = childValue;
-                bestMove = currPrettyMove;
-            }
-            if (childValue < beta)
-            {
-                beta = childValue;
-            }
-        }
-
-        // Alpha-beta pruning
-        if (alpha >= beta)
-        {
-            break;
-        }
-    }
-
-    if (isMaximizingPlayer)
-    {
-        return [bestMove, maxValue]
-    }
-    else
-    {
-        return [bestMove, minValue];
-    }
-}
-
 function checkStatus (color) {
     if (game.in_checkmate())
     {
@@ -352,57 +262,7 @@ function checkStatus (color) {
     return true;
 }
 
-function updateAdvantage()
-{
-    if (globalSum > 0)
-    {
-        $('#advantageColor').text('Black');
-        $('#advantageNumber').text(globalSum);
-    }
-    else if (globalSum < 0)
-    {
-        $('#advantageColor').text('White');
-        $('#advantageNumber').text(-globalSum);
-    }
-    else
-    {
-        $('#advantageColor').text('Neither side');
-        $('#advantageNumber').text(globalSum);
-    }
-    $('#advantageBar').attr({
-        "aria-valuenow": `${-globalSum}`,
-        style: `width: ${(-globalSum + 2000) / 4000 * 100}%`,
-    });
-}
 
-/*
- * Calculates the best legal move for the given color.
- */
-function getBestMove (game, color, currSum) {
-
-    positionCount = 0;
-    
-    if (color === 'b')
-    {
-        var depth = parseInt($('#search-depth').find(':selected').text());
-    }
-    else
-    {
-        var depth = parseInt($('#search-depth-white').find(':selected').text());
-    }
-
-    var d = new Date().getTime();
-    var [bestMove, bestMoveValue] = minimax(game, depth, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY, true, currSum, color);
-    var d2 = new Date().getTime();
-    var moveTime = (d2 - d);
-    var positionsPerS = (positionCount * 1000 / moveTime);
-
-    $('#position-count').text(positionCount);
-    $('#time').text(moveTime/1000);
-    $('#positions-per-s').text(Math.round(positionsPerS));
-
-    return [bestMove, bestMoveValue];
-}
 
 function loadPGN() {
 jQuery.get('https://arunmoorthattil.github.io/chess-puzzle/css/pgn_data.pgn', function (data) {
@@ -509,17 +369,6 @@ $('#Next').on('click', function() {
          board.position(game.fen());
 	window.setTimeout(function() {makeBestMove(game.turn())}, 250)
 })
-$('#startBtn').on('click', function() {
-    reset();
-})
-
-$('#compVsCompBtn').on('click', function() {
-    reset();
-    compVsComp('w');
-})
-$('#resetBtn').on('click', function() {
-    reset();
-})
 
 
 var undo_stack = [];
@@ -556,29 +405,6 @@ $('#undoBtn').on('click', function() {
     {
         alert("Nothing to undo.");
     }  
-})
-
-function redo()
-{
-    game.move(undo_stack.pop());
-    board.position(game.fen());
-}
-
-$('#redoBtn').on('click', function() {
-
-    if (undo_stack.length >= 2)
-    {
-        // Redo twice: Player's last move, followed by opponent's last move
-        redo();
-        window.setTimeout(function(){
-            redo();
-            window.setTimeout(function () {showHint()}, 250)
-        }, 250);
-    }
-    else
-    {
-        alert("Nothing to redo.");
-    }
 })
 
 
@@ -627,10 +453,7 @@ function onDrop (source, target) {
     // Illegal move
     if (move === null) return 'snapback'
     
-    globalSum = evaluateBoard(move, globalSum, 'b');
-    updateAdvantage();
-
-    // Highlight latest move
+        // Highlight latest move
     $board.find('.' + squareClass).removeClass('highlight-white')
     
     $board.find('.square-' + move.from).addClass('highlight-white')
