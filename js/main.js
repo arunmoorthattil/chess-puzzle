1199e31b99229408f455be8616b43ac8abd05d66
arunmoorthattil <43172183+arunmoorthattil@users.noreply.github.com>
2021-05-10 18:16:32 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -162,72 +162,6 @@ var pst_b = {
 var pstOpponent = {'w': pst_b, 'b': pst_w};
 var pstSelf = {'w': pst_w, 'b': pst_b};
 
-/* 
- * Evaluates the board at this point in time, 
- * using the material weights and piece square tables.
- */
-function evaluateBoard (move, prevSum, color) 
-{
-    var from = [8 - parseInt(move.from[1]), move.from.charCodeAt(0) - 'a'.charCodeAt(0)];
-    var to = [8 - parseInt(move.to[1]), move.to.charCodeAt(0) - 'a'.charCodeAt(0)];
-
-    // Change endgame behavior for kings
-    if (prevSum < -1500)
-    {
-        if (move.piece === 'k') {move.piece = 'k_e'}
-        else if (move.captured === 'k') {move.captured = 'k_e'}
-    }
-
-    if ('captured' in move)
-    {
-        // Opponent piece was captured (good for us)
-        if (move.color === color)
-        {
-            prevSum += (weights[move.captured] + pstOpponent[move.color][move.captured][to[0]][to[1]]);
-        }
-        // Our piece was captured (bad for us)
-        else
-        {
-            prevSum -= (weights[move.captured] + pstSelf[move.color][move.captured][to[0]][to[1]]);
-        }
-    }
-
-    if (move.flags.includes('p'))
-    {
-        // NOTE: promote to queen for simplicity
-        move.promotion = 'q';
-
-        // Our piece was promoted (good for us)
-        if (move.color === color)
-        {
-            prevSum -= (weights[move.piece] + pstSelf[move.color][move.piece][from[0]][from[1]]);
-            prevSum += (weights[move.promotion] + pstSelf[move.color][move.promotion][to[0]][to[1]]);
-        }
-        // Opponent piece was promoted (bad for us)
-        else
-        {
-            prevSum += (weights[move.piece] + pstSelf[move.color][move.piece][from[0]][from[1]]);
-            prevSum -= (weights[move.promotion] + pstSelf[move.color][move.promotion][to[0]][to[1]]);
-        }
-    }
-    else
-    {
-        // The moved piece still exists on the updated board, so we only need to update the position value
-        if (move.color !== color)
-        {
-            prevSum += pstSelf[move.color][move.piece][from[0]][from[1]];
-            prevSum -= pstSelf[move.color][move.piece][to[0]][to[1]];
-        }
-        else
-        {
-            prevSum -= pstSelf[move.color][move.piece][from[0]][from[1]];
-            prevSum += pstSelf[move.color][move.piece][to[0]][to[1]];
-        }
-    }
-    
-    return prevSum;
-}
-
 function checkStatus (color) {
     if (game.in_checkmate())
     {
@@ -279,15 +213,15 @@ jQuery.get('https://arunmoorthattil.github.io/chess-puzzle/css/pgn_data.pgn', fu
  * Makes the best legal move for the given color.
  */
 function makeBestMove(color) {
-     var mv=positions[currPos].moves[currMov];
-	console.log(currMov)
-	console.log(mv)
-        currMov+=2;
+	console.log(currPos+' moves'+ currMov);
+       var mv=positions[currPos].moves[currMov];
+	console.log(currMov);
+	console.log(mv);
         var move= game.move(mv);
 	console.log(move);
 	console.log(color)
     board.position(game.fen());
-    if (color === 'black')
+    if (color ==='b')
     {
         checkStatus('black');
 
@@ -425,7 +359,7 @@ function onDrop (source, target) {
 
            // Make the best move for black
         window.setTimeout(function() {
-            makeBestMove(game.turn);
+            makeBestMove(game.turn());
         }, 250)
     }
 
